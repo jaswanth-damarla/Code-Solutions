3a51a9a7d9e8b1802b7c67ac28e9f210fa04f136
Jaswanth <86583861+jaswanth-damarla@users.noreply.github.com>
2024-10-19 09:22:02 +0530

    Add files via upload
    sum of first n natural numbers

---
+++ sum_of_naturalnum.py
@@ -0,0 +1,10 @@
+n = int(input("Enter the natural number: "))
+
+# Initialize sum to 0
+sum = 0
+
+# Loop through numbers from 1 to n (inclusive)
+for i in range(1, n + 1):
+    sum += i
+
+print("Sum:", sum)
