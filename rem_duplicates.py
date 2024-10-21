2872c7ddee3042eac9c85fda5daa910b5e24a781
Jaswanth <86583861+jaswanth-damarla@users.noreply.github.com>
2024-10-21 09:24:01 +0530

    rem_duplicates.py
    Removing duplicates from a list

---
+++ rem_duplicates.py
@@ -0,0 +1,8 @@
+n = [2, 3, 3, 4, 2, 7, 8, 9, 5, 6, 4, 8, 7, 5]
+
+# Convert the list to a set to remove duplicates
+m = set(n)
+
+# Convert back to a list (if needed) and print the result
+m = list(m)
+print(m)
